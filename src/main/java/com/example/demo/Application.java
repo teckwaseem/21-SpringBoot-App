243f2cacca11dd4dc2a9e1908b5f6e68bd024b9b
waseem <waseem@gmail.com>
2020-08-24 15:48:14 +0530

    HIS-134 fix provided

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		// changes 134
 		
 		run.close();
 	}
